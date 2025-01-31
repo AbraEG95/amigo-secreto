a501ca519a3818e9808dbe6c558068714dba005f
AbraEG95 <abrajamguerre1995@gmail.com>
2025-01-31 01:31:44 -0600

    Implementa una función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/*El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+Aquí deberás desarrollar la lógica para resolver el problema.*/
+
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre =  document.getElementById('amigo').value;
+    let mayusculas = nombre.toUpperCase();
+    //console.log(nombre);
+    if (nombre === '') {
+        //Alerta vacio
+        alert('Por favor, inserte un nombre.');
+        return;
+    }else{
+        if (amigos.includes(mayusculas)) {
+            return;
+        }
+        amigos.push(mayusculas);
+        console.log(`Amigo agregado: ${mayusculas}`);
+        console.log(`Lista de amigos:`, amigos);
+        //console.log(amigos.length);
+    }
+    //Limpia el input
+    document.getElementById('amigo').value = '';
+}
+
+
